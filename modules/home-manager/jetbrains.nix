131510557e50cf689c6c75b36fb1c55149fd714a
FinnPL <contact@lippok.eu>
2025-06-10 00:28:52 +0200

    test3

--- modules/home-manager/jetbrains.nix
+++ modules/home-manager/jetbrains.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 let
-  clionWithCopilot = pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.clion [
-    "github-copilot"
-  ];
-in
-{
-  home.packages = with pkgs; [
+  clionWithCopilot =
+    pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.clion [
+      pkgs.jetbrains.plugins.github-copilot
+    ];
+in {
+  home.packages = [
     clionWithCopilot
   ];
 }
