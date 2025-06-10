508f6ff039b989f11fa0dafb1e2710c24eaca8f8
FinnPL <contact@lippok.eu>
2025-06-10 15:34:08 +0200

    feat: add automatic updates and cleanup configuration

---
+++ modules/nixos/auto.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, ...}:
+
+{
+  # Automatic Updates
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.dates = "weekly";
+
+  # Automatic cleanup
+  nix.gc.automatic = true;
+  nix.gc.dates = "weekly";
+  nix.gc.options = "--delete-older-than 10d";
+  nix.settings.auto-optimise-store = true;
+}
