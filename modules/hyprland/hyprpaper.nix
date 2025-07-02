e52a54ec33c85d61b8116cbcc45c2e40163b2fb4
FinnPL <contact@lippok.eu>
2025-07-02 22:59:09 +0200

    fix: update wallpaper file format from jpg to png in hyprpaper configuration

--- modules/hyprland/hyprpaper.nix
+++ modules/hyprland/hyprpaper.nix
@@ -6,7 +6,7 @@
   ];
 
   xdg.configFile."hypr/hyprpaper.conf".text = ''
-    preload = ${config.home.homeDirectory}/Pictures/wallpaper.jpg
-    wallpaper = eDP-1,${config.home.homeDirectory}/Pictures/wallpaper.jpg
+    preload = ${config.home.homeDirectory}/Pictures/wallpaper.png
+    wallpaper = eDP-1,${config.home.homeDirectory}/Pictures/wallpaper.png
   '';
 }
