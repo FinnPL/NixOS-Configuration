78643e2ece7993344b95d46f5d69a1cb1cd1e461
FinnPL <contact@lippok.eu>
2025-07-02 23:51:12 +0200

    fix: update nautilus package reference and refactor hyprland configuration for clarity

--- modules/hyprland/hyprland-config.nix
+++ modules/hyprland/hyprland-config.nix
@@ -5,75 +5,71 @@
     jq
   ];
 
-  wayland.windowManager.hyprland = {
-    enable = true;
+  home.file.".config/hypr/hyprland.conf".text = ''
+    misc {
+      disable_hyprland_logo = true
+    }
+    exec-once = hyprpaper
+    exec-once = waybar
+    exec-once = mako
+    exec-once = wl-paste --watch cliphist store
 
-    extraConfig = ''
-      misc {
-        disable_hyprland_logo = true
+    monitor = eDP-1,1920x1080@60,0x0,1
+
+    # Set Swiss German keyboard layout and enable natural scrolling
+    input {
+      kb_layout = ch
+      kb_variant = de
+      
+      touchpad {
+        natural_scroll = yes
       }
-      exec-once = hyprpaper
-      exec-once = waybar
-      exec-once = mako
-      exec-once = wl-paste --watch cliphist store
+    }
 
-      monitor = eDP-1,1920x1080@60,0x0,1
+    dwindle {
+      preserve_split = true
+    }
 
-      # Set Swiss German keyboard layout and enable natural scrolling
-      input {
-        kb_layout = ch
-        kb_variant = de
-        
-        touchpad {
-          natural_scroll = yes
-        }
-      }
+    $mod = SUPER
+    $term = kitty
+    $browser = firefox
 
-      dwindle {
-        preserve_split = true
-      }
+    # Rofi toggle on Alt+Space
+    bind = ALT, SPACE, exec, pkill rofi || rofi -show drun -no-config -theme-str '@theme "${config.home.homeDirectory}/.config/rofi/adv.rasi"'
+    bind = $mod, V, exec, pkill rofi || (cliphist list | rofi -dmenu | cliphist decode | wl-copy)
 
-      $mod = SUPER
-      $term = kitty
-      $browser = firefox
+    # Terminal & browser
+    bind = $mod, SPACE, exec, $term
+    bind = $mod, F, exec, $browser
 
-      # Rofi toggle on Alt+Space
-      bind = ALT, SPACE, exec, pkill rofi || rofi -show drun -no-config -theme-str '@theme "${config.home.homeDirectory}/.config/rofi/adv.rasi"'
-      bind = $mod, V, exec, pkill rofi || (cliphist list | rofi -dmenu | cliphist decode | wl-copy)
+    # Move focus to different tile
+    bind = ALT, left, movefocus, l
+    bind = ALT, right, movefocus, r
+    bind = ALT, up, movefocus, u
+    bind = ALT, down, movefocus, d
 
-      # Terminal & browser
-      bind = $mod, SPACE, exec, $term
-      bind = $mod, F, exec, $browser
+    # Resize window
+    bind = $mod+CTRL, left, resizeactive, -30 0
+    bind = $mod+CTRL, right, resizeactive, 30 0
+    bind = $mod+CTRL, up, resizeactive, 0 -30
+    bind = $mod+CTRL, down, resizeactive, 0 30
 
-      # Move focus to different tile
-      bind = ALT, left, movefocus, l
-      bind = ALT, right, movefocus, r
-      bind = ALT, up, movefocus, u
-      bind = ALT, down, movefocus, d
+    # Move between workspaces
+    bind = $mod+ALT, left, workspace, -1
+    bind = $mod+ALT, right, workspace, +1
 
-      # Resize window
-      bind = $mod+CTRL, left, resizeactive, -30 0
-      bind = $mod+CTRL, right, resizeactive, 30 0
-      bind = $mod+CTRL, up, resizeactive, 0 -30
-      bind = $mod+CTRL, down, resizeactive, 0 30
+    # Close window with ALT+Q
+    bind = ALT, Q, killactive, 
 
-      # Move between workspaces
-      bind = $mod+ALT, left, workspace, -1
-      bind = $mod+ALT, right, workspace, +1
+    # Move active window (with edge detection)
+    bind = $mod, left, exec, ~/.config/hypr/move_or_switch.sh left
+    bind = $mod, right, exec, ~/.config/hypr/move_or_switch.sh right
+    bind = $mod, up, exec, ~/.config/hypr/move_or_switch.sh up
+    bind = $mod, down, exec, ~/.config/hypr/move_or_switch.sh down
 
-      # Close window with ALT+Q
-      bind = ALT, Q, killactive, 
-
-      # Move active window (with edge detection)
-      bind = $mod, left, exec, ~/.config/hypr/move_or_switch.sh left
-      bind = $mod, right, exec, ~/.config/hypr/move_or_switch.sh right
-      bind = $mod, up, exec, ~/.config/hypr/move_or_switch.sh up
-      bind = $mod, down, exec, ~/.config/hypr/move_or_switch.sh down
-
-      # Logout with SUPER+SHIFT+L
-      bind = $mod+SHIFT, L, exec, hyprctl dispatch exit
-    '';
-  };
+    # Logout with SUPER+SHIFT+L
+    bind = $mod+SHIFT, L, exec, hyprctl dispatch exit
+  '';
 
   home.file = {
     ".config/hypr/move_or_switch.sh" = {
